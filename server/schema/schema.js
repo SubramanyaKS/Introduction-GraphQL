e570f790337dd62b15d3778b92caa03749f2a02d
Subramanya K S <subramanyaks2205@gmail.com>
2022-10-24 20:05:13 +0530

    updated lesson 12

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql');
 const _= require('lodash');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema,GraphQLID}=graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema,GraphQLID,GraphQLInt}=graphql;
 
 // dummy data
 
@@ -10,6 +10,12 @@ var books = [
     {name: 'The Final Empire', genre:'Fantasy', id:'2'},
     {name: 'The Long Earth', genre:'So-Fi', id:'3'},
 
+];
+
+var authors = [
+    { name: 'Patrick Rothfuss', age: 44, id: '1' },
+    { name: 'Brandon Sanderson', age: 42, id: '2' },
+    { name: 'Terry Pratchett', age: 66, id: '3' }
 ];
 
 const BookType = new GraphQLObjectType({
@@ -22,16 +28,31 @@ const BookType = new GraphQLObjectType({
  })
 });
 
-const RootQuery = new GraphQLObjectType({
-    name:'RootQueryType',
-    fields:{
-        book:{
-            type:BookType,
-            args:{id:{type:GraphQLID}},
-            resolve(parent,args){
-                console.log(typeof(args.id));
-               return  _.find(books,{id:args.id})
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: ( ) => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+    })
+});
 
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args){
+                // code to get data from db / other source
+                return _.find(books, { id: args.id });
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args){
+                return _.find(authors, { id: args.id });
             }
         }
     }
